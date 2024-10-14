987ce993acc3db4b9b3ba80d47a69c23836cbebf
Dylan Johnson <marcusdylanjohnson@gmail.com>
2024-10-14 14:23:59 -0400

    feat(nvim): configure JS/TS formatters

--- nvim/dot-config/nvim/lua/plugins/formatting.lua
+++ nvim/dot-config/nvim/lua/plugins/formatting.lua
@@ -1,13 +1,62 @@
+local js_formatters = { 'biome', 'eslint_d', 'prettierd' }
+
+---@type LazyPluginSpec
 return {
 	'stevearc/conform.nvim',
 	event = { 'BufWritePre' },
 	cmd = { 'ConformInfo' },
-	---@module "conform"
-	---@type conform.setupOpts
-	opts = {
-		formatters_by_ft = {
-			lua = { 'stylua' },
-		},
-		format_on_save = { timeout_ms = 500 },
-	},
+	config = function()
+		-- same as the configuration from the plugin, minus `package.json` as we want to be more specific
+		local prettier_cwd = require('conform.util').root_file {
+			'.prettierrc',
+			'.prettierrc.json',
+			'.prettierrc.yml',
+			'.prettierrc.yaml',
+			'.prettierrc.json5',
+			'.prettierrc.js',
+			'.prettierrc.cjs',
+			'.prettierrc.mjs',
+			'.prettierrc.toml',
+			'prettier.config.js',
+			'prettier.config.cjs',
+			'prettier.config.mjs',
+		}
+
+		local eslint_cwd = require('conform.util').root_file {
+			'.eslintrc',
+			'.eslintrc.js',
+			'.eslintrc.cjs',
+			'.eslintrc.mjs',
+			'.eslintrc.json',
+			'eslint.config.js',
+		}
+
+		require('conform').setup {
+			formatters_by_ft = {
+				lua = { 'stylua' },
+				javascript = js_formatters,
+				javascriptreact = js_formatters,
+				typescript = js_formatters,
+				typescriptreact = js_formatters,
+			},
+			format_on_save = { timeout_ms = 5000, lsp_format = 'fallback' },
+			formatters = {
+				biome = {
+					require_cwd = true,
+				},
+				eslint_d = {
+					cwd = function(self, ctx)
+						if not prettier_cwd(self, ctx) then
+							return eslint_cwd(self, ctx)
+						end
+					end,
+					require_cwd = true,
+				},
+				prettierd = {
+					cwd = prettier_cwd,
+					require_cwd = true,
+				},
+			},
+		}
+	end,
 }
