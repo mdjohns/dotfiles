e2edcd8f603bd38dfe443e94fedc0a039fa8d248
Dylan Johnson <marcusdylanjohnson@gmail.com>
2024-10-24 13:56:30 -0400

    feat(nvim): update telescope theme

--- nvim/dot-config/nvim/lua/plugins/ui.lua
+++ nvim/dot-config/nvim/lua/plugins/ui.lua
@@ -73,7 +73,16 @@ return {
 		name = 'rose-pine',
 		priority = 1000,
 		config = function()
-			require('rose-pine').setup {}
+			require('rose-pine').setup {
+				highlight_groups = {
+					TelescopeBorder = { fg = 'highlight_high', bg = 'none' },
+					TelescopeNormal = { bg = 'none' },
+					TelescopePromptNormal = { bg = 'base' },
+					TelescopeResultsNormal = { fg = 'subtle', bg = 'none' },
+					TelescopeSelection = { fg = 'text', bg = 'base' },
+					TelescopeSelectionCaret = { fg = 'rose', bg = 'rose' },
+				},
+			}
 
 			vim.cmd.colorscheme 'rose-pine'
 		end,
