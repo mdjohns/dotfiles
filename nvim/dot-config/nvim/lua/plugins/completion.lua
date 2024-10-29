5fbffd040e6154def836c164458a156634915133
Dylan Johnson <marcusdylanjohnson@gmail.com>
2024-10-29 09:01:51 -0400

    feat(nvim): upgrade `blink.cmp` / remove `nvim.cmp`

--- nvim/dot-config/nvim/lua/plugins/completion.lua
+++ nvim/dot-config/nvim/lua/plugins/completion.lua
@@ -7,11 +7,11 @@ return {
 		---@type blink.cmp.Config
 		opts = {
 			keymap = {
-				accept = '<C-y>',
-				select_next = '<C-n>',
-				select_prev = '<C-p>',
-				snippet_forward = '<C-j>',
-				snippet_backward = '<C-k>',
+				['<C-y>'] = { 'accept' },
+				['<C-n>'] = { 'select_next' },
+				['<C-p>'] = { 'select_prev' },
+				['<C-j>'] = { 'snippet_forward' },
+				['<C-k>'] = { 'snippet_backward' },
 			},
 			windows = {
 				autocomplete = {
@@ -21,58 +21,5 @@ return {
 			},
 		},
 		config = true,
-	},
-	---@type LazyPluginSpec
-	{
-		enabled = false,
-		'iguanacucumber/magazine.nvim',
-		dependencies = {
-			{ 'neovim/nvim-lspconfig' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/cmp-buffer' },
-			{ 'hrsh7th/cmp-path' },
-			{ 'hrsh7th/cmp-cmdline' },
-			{ 'onsails/lspkind.nvim' },
-		},
-		config = function()
-			local cmp = require 'cmp'
-			local lspkind = require 'lspkind'
-
-			cmp.setup {
-				formatting = {
-					format = lspkind.cmp_format {
-						mode = 'symbol_text',
-						maxwidth = 50,
-						ellipsis_char = '...',
-						show_labelDetails = true,
-					},
-				},
-				snippet = {
-					expand = function(args)
-						vim.snippet.expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert {
-					['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-					['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-					['<C-y>'] = cmp.mapping(
-						cmp.mapping.confirm {
-							behavior = cmp.ConfirmBehavior.Insert,
-							select = true,
-						},
-						{ 'i', 'c' }
-					),
-				},
-				sources = {
-					{ name = 'nvim_lsp' },
-					{ name = 'path' },
-					{ name = 'buffer' },
-				},
-			}
-		end,
 	},
 }
