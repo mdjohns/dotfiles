d5b9a2d60d2ed95e41c2ac99a5076cf85383caae
Dylan Johnson <marcusdylanjohnson@gmail.com>
2024-11-08 15:14:00 -0500

    chore(nvim): remove unneeded log from `filetype.lua`

--- nvim/dot-config/nvim/lua/config/filetype.lua
+++ nvim/dot-config/nvim/lua/config/filetype.lua
@@ -5,7 +5,6 @@ vim.filetype.add {
 		-- this function replaces the `dot-`prefix with a `.` to allow neovim to properly resolve the filetype
 		['dot-.*'] = function(_, _, ext)
 			local actual_file_name = string.gsub(ext, 'dot%-(.*)', '.%1')
-			vim.notify(actual_file_name, vim.log.levels.INFO)
 			return vim.filetype.match { filename = actual_file_name }
 		end,
 	},
