bb99ed81b62931709825f0d072e68221fcc19f77
Dylan Johnson <marcusdylanjohnson@gmail.com>
2024-12-02 17:02:39 -0500

    feat(nvim): add command to disable formatting

--- nvim/dot-config/nvim/lua/plugins/formatting.lua
+++ nvim/dot-config/nvim/lua/plugins/formatting.lua
@@ -32,6 +32,27 @@ return {
 			'eslint.config.mjs',
 		}
 
+		vim.api.nvim_create_user_command('FormatDisable', function(args)
+			if args.bang then
+				-- FormatDisable! to disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				-- Otherwise disable it globally
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = 'Disable format on save',
+			bang = true,
+		})
+
+		vim.api.nvim_create_user_command('FormatEnable', function()
+			vim.g.disable_autoformat = true
+			vim.b.disable_autoformat = true
+		end, {
+			desc = 'Re-enable format on save',
+			bang = true,
+		})
+
 		require('conform').setup {
 			formatters_by_ft = {
 				astro = js_formatters,
@@ -47,7 +68,13 @@ return {
 				typescriptreact = js_formatters,
 				yaml = { 'prettierd' },
 			},
-			format_on_save = { timeout_ms = 5000, lsp_format = 'fallback' },
+			format_on_save = function(bufnr)
+				-- Disable formatting on save with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 1000, lsp_format = 'fallback' }
+			end,
 			formatters = {
 				biome = {
 					require_cwd = true,
