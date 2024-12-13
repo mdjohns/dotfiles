05ae3fc822a72df9e138a1d6642d4f523a247c53
Dylan Johnson <marcusdylanjohnson@gmail.com>
2024-12-13 09:19:17 -0500

    feat(nvim): update deps / update blink config to latest format

--- nvim/dot-config/nvim/lua/plugins/completion.lua
+++ nvim/dot-config/nvim/lua/plugins/completion.lua
@@ -6,17 +6,27 @@ return {
 		build = 'cargo build --release',
 		---@type blink.cmp.Config
 		opts = {
+			---@diagnostic disable: missing-fields
+			completion = {
+				documentation = {
+					window = {
+						border = 'rounded',
+					},
+				},
+				ghost_text = {
+					enabled = true,
+				},
+				menu = {
+					border = 'rounded',
+				},
+				---@diagnostic enable: missing-fields
+			},
 			keymap = {
 				['<C-y>'] = { 'accept' },
 				['<C-n>'] = { 'select_next' },
 				['<C-p>'] = { 'select_prev' },
 				['<C-j>'] = { 'snippet_forward' },
 				['<C-k>'] = { 'snippet_backward' },
-			},
-			windows = {
-				autocomplete = {
-					border = 'rounded',
-				},
 			},
 		},
 		config = true,
