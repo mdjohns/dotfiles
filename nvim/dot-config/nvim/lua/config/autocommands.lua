8283e0f1f9578a1468b3fadd181c3192c48676df
Dylan Johnson <marcusdylanjohnson@gmail.com>
2025-01-17 16:15:53 -0500

    feat(nvim): add autcommand to restart `prettierd`

--- nvim/dot-config/nvim/lua/config/autocommands.lua
+++ nvim/dot-config/nvim/lua/config/autocommands.lua
@@ -1,7 +1,19 @@
-local yank_group = vim.api.nvim_create_augroup('HighlightYank', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-- Highlight yanks
+autocmd('TextYankPost', {
 	callback = function()
 		vim.highlight.on_yank { higroup = 'Visual', timeout = 200 }
 	end,
-	group = yank_group,
+	group = augroup('HighlightYank', { clear = true }),
+})
+
+-- Restart `prettierd` when a prettier config file has been changed
+autocmd({ 'BufWritePost' }, {
+	callback = function()
+		vim.fn.system 'prettierd restart'
+	end,
+	group = augroup('RestartPrettierd', { clear = true }),
+	pattern = '*prettier*',
 })
