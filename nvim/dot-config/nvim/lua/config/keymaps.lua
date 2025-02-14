e5d1c3cbe75ee38d68c290477bf0c12542d8dc01
Dylan Johnson <marcusdylanjohnson@gmail.com>
2025-02-14 13:05:10 -0500

    feat(nvim): keymaps for resizing splits

--- nvim/dot-config/nvim/lua/config/keymaps.lua
+++ nvim/dot-config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,8 @@ vim.g.mapleader = ' '
 
 set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
 set({ 'n', 'v' }, '<leader>p', '"+p', { desc = 'Paste from system clipboard' })
+
+set('n', '<M-,>', '<c-w>5<', { desc = 'Decrease width of current split' })
+set('n', '<M-.>', '<c-w>5>', { desc = 'Increase width of current split' })
+set('n', '<M-t>', '<c-w>+', { desc = 'Make current split [t]aller' })
+set('n', '<M-s>', '<c-w>-', { desc = 'Make current split [s]horter' })
