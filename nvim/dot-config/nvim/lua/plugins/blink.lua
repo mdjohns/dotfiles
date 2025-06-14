9090cf69d44d2ead2e9378f9df7a626e64d909de
Dylan Johnson <marcusdylanjohnson@gmail.com>
2025-06-13 21:25:15 -0500

    chore(nvim): rename blink plugin config

---
+++ nvim/dot-config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,60 @@
+---@type LazyPluginSpec
+return {
+
+	'saghen/blink.cmp',
+	lazy = false,
+	build = 'cargo build --release',
+	dependencies = {
+		{ 'rafamadriz/friendly-snippets' },
+		{ 'giuxtaposition/blink-cmp-copilot' },
+		{ 'zbirenbaum/copilot.lua' },
+	},
+	---@type blink.cmp.Config
+	opts = {
+		cmdline = {
+			completion = {
+				menu = {
+					auto_show = true,
+				},
+			},
+			keymap = {
+				preset = 'inherit',
+			},
+		},
+		completion = {
+			documentation = {
+				window = {
+					border = 'rounded',
+				},
+			},
+			ghost_text = {
+				enabled = true,
+			},
+			menu = {
+				border = 'rounded',
+				auto_show = function(_)
+					return vim.bo.filetype ~= 'TelescopePrompt'
+				end,
+			},
+		},
+		keymap = {
+			['<C-y>'] = { 'accept' },
+			['<C-n>'] = { 'select_next' },
+			['<C-p>'] = { 'select_prev' },
+			['<C-j>'] = { 'snippet_forward' },
+			['<C-k>'] = { 'snippet_backward' },
+		},
+		sources = {
+			default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot', 'codecompanion' },
+			providers = {
+				copilot = {
+					name = 'copilot',
+					module = 'blink-cmp-copilot',
+					score_offset = 100,
+					async = true,
+				},
+			},
+		},
+	},
+	config = true,
+}
