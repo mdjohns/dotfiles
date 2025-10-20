27ddef212d8dad19161f4c564a2c485ea3f2cb2d
Dylan Johnson <marcusdylanjohnson@gmail.com>
2025-10-20 15:13:28 -0400

    feat(nvim): add kulala keybinds

--- nvim/dot-config/nvim/lua/plugins/kulala.lua
+++ nvim/dot-config/nvim/lua/plugins/kulala.lua
@@ -1,32 +1,35 @@
 ---@type LazyPluginSpec
 return {
-		'mistweaverco/kulala.nvim',
-		keys = {
-			{
-				'<leader>kk',
-				function()
-					require('kulala').run()
-				end,
-				desc = 'Send HTTP request',
-			},
-			{
-				'<leader>ka',
-				function()
-					require('kulala').run_all()
-				end,
-				desc = 'Send all HTTP requests in the current buffer',
-			},
-			{
-				'<leader>kp',
-				function()
-					require('kulala').scratchpad()
-				end,
-				desc = 'Open Kulala scratchpad',
-			},
+	'mistweaverco/kulala.nvim',
+	keys = {
+		{
+			'<leader>kk',
+			function()
+				require('kulala').run()
+			end,
+			desc = 'Send HTTP request',
 		},
-		ft = { 'http', 'rest' },
-		opts = {
-			additional_curl_options = { '--insecure' },
-			global_keymaps = false,
+		{
+			'<leader>ka',
+			function()
+				require('kulala').run_all()
+			end,
+			desc = 'Send all HTTP requests in the current buffer',
 		},
+		{
+			'<leader>kp',
+			function()
+				require('kulala').scratchpad()
+			end,
+			desc = 'Open Kulala scratchpad',
+		},
+	},
+	ft = { 'http', 'rest' },
+	opts = {
+		additional_curl_options = { '--insecure' },
+		global_keymaps = false,
+		lsp = {
+			keymaps = true,
+		},
+	},
 }
