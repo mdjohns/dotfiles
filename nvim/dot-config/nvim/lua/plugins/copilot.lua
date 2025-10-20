1abd9f484f178f9837a9efed13f0661b668723c5
Dylan Johnson <marcusdylanjohnson@gmail.com>
2025-10-20 15:13:10 -0400

    feat(nvim): use node 22 for copilot

--- nvim/dot-config/nvim/lua/plugins/copilot.lua
+++ nvim/dot-config/nvim/lua/plugins/copilot.lua
@@ -4,16 +4,16 @@ return {
 		'zbirenbaum/copilot.lua',
 		cmd = 'Copilot',
 		event = 'InsertEnter',
-		opts = {
-			panel = {
-				enabled = false,
-			},
-			suggestion = {
-				enabled = false,
-			},
-		},
-		config = function(opts)
-			require('copilot').setup(opts)
+		config = function()
+			require('copilot').setup {
+				copilot_node_command = vim.fn.expand '$FNM_DIR' .. '/node-versions/v22.19.0/installation/bin/node',
+				panel = {
+					enabled = false,
+				},
+				suggestion = {
+					enabled = false,
+				},
+			}
 		end,
 	},
 	{
