1e34f90391bf5f3b2333a1d8a13f4479ce08fd76
Dylan Johnson <marcusdylanjohnson@gmail.com>
2025-12-04 08:59:37 -0500

    feat(nvim): use floating windows

--- nvim/dot-config/nvim/lua/plugins/neogit.lua
+++ nvim/dot-config/nvim/lua/plugins/neogit.lua
@@ -14,7 +14,15 @@ return {
 		'ibhagwan/fzf-lua',
 	},
 	opts = {
+		auto_show_console = false,
+		graph_style = 'kitty',
 		kind = 'floating',
+		commit_editor = {
+			kind = 'floating',
+		},
+		rebase_editor = {
+			kind = 'floating',
+		},
 	},
 	config = true,
 }
