54e4f6d2076ae9390c79ff4c967c8ba6c4a88a94
Dylan Johnson <marcusdylanjohnson@gmail.com>
2026-04-14 15:33:26 -0400

    fix(nvim): configure treesitter correctly

--- nvim/dot-config/nvim/lua/plugins/treesitter.lua
+++ nvim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -2,4 +2,59 @@ return {
 	'nvim-treesitter/nvim-treesitter',
 	lazy = false,
 	build = ':TSUpdate',
+	config = function()
+		local ts = require 'nvim-treesitter'
+		local parsers = {
+			'bash',
+			'css',
+			'comment',
+			'dockerfile',
+			'diff',
+			'git_config',
+			'gitcommit',
+			'gitignore',
+			'html',
+			'http',
+			'javascript',
+			'jsdoc',
+			'json',
+			'json5',
+			'lua',
+			'markdown',
+			'markdown_inline',
+			'terraform',
+			'toml',
+			'tsx',
+			'typescript',
+			'vim',
+			'vimdoc',
+			'yaml',
+		}
+
+		for _, parser in ipairs(parsers) do
+			ts.install(parser)
+		end
+
+		local patterns = {}
+		for _, parser in ipairs(parsers) do
+			local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+
+			for _, pattern in ipairs(parser_patterns) do
+				table.insert(patterns, pattern)
+			end
+		end
+
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = patterns,
+			callback = function()
+				-- highlighting
+				vim.treesitter.start()
+				-- indentation
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				-- folds
+				vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+				vim.wo[0][0].foldmethod = 'expr'
+			end,
+		})
+	end,
 }
