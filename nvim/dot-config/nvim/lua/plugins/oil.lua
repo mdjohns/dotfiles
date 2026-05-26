d33229d5589925c03729968318143aa567a313e7
marcus dylan johnson <marcusdylanjohnson@gmail.com>
2026-05-26 09:23:39 -0400

    feat(nvim): add oil mappings for yanks

--- nvim/dot-config/nvim/lua/plugins/oil.lua
+++ nvim/dot-config/nvim/lua/plugins/oil.lua
@@ -13,6 +13,19 @@ return {
 					return true
 				end,
 			},
+			keymaps = {
+				['<leader>y'] = {
+					'actions.yank_entry',
+					desc = 'Yank file path',
+				},
+				['<leader>Y'] = {
+					function()
+						require('oil.actions').yank_entry.callback()
+						vim.fn.setreg('+', vim.fn.getreg('"'))
+					end,
+					desc = 'Copy file path to system clipboard',
+				},
+			},
 		}
 		vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 	end,
